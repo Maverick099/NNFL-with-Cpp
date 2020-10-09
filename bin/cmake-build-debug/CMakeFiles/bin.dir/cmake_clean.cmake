5578c28d44ab89569458a2075859fd4b7594a859
MavericK099 <adithyashetty032@gmail.com>
2020-10-09 17:34:55 +0530

    unversioned files

---
+++ bin/cmake-build-debug/CMakeFiles/bin.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bin.dir/main.cpp.obj"
+  "bin.exe"
+  "bin.exe.manifest"
+  "bin.lib"
+  "bin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
