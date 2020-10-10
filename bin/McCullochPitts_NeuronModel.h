9322cd135a00f6285c3ffda508da189ad6b55455
MavericK099 <adithyashetty032@gmail.com>
2020-10-10 20:34:52 +0530

    Not Gate workin g

--- bin/McCullochPitts_NeuronModel.h
+++ bin/McCullochPitts_NeuronModel.h
@@ -8,37 +8,41 @@
 #ifndef BIN_MCCULLOCHPITTS_NEURONMODEL_H
 #define BIN_MCCULLOCHPITTS_NEURONMODEL_H
 
-#include <cassert>
 #include <iostream>
-#include <math.h>
+#include <string>
+#include <cassert>
+#include <cmath>
+#include <utility>
 
 using namespace std;
 
-int notGate(int input) {
-    assert(input == 1 || input == 0);
-    cout << "[!] Input is not equal to 1 or 0" << endl;
+// Default weight and threshold values
+unsigned int threshold_value = 15;
+unsigned int weight1 = -10;
+unsigned int weight2 = 10;
+string activationFunc;
 
-
-    return 0;
-
+string transformToLowerCase(string text) {
+    string returnText;
+    int len = text.length();
+    for (int i = 0; i < len; i++) {
+        returnText[i] = tolower(text[i]);
+    }
+    cout << returnText << endl;
+    return returnText;
 }
 
-
-int andGate(int input1, int input2) {
-
-
-}
 
 /// Different Activation function
-// 1) Linear Activation function
-// 2) Threshold Activation function
-// 3) Sigmoid Activation function
+// 0) Linear Activation function
+// 1) Threshold Activation function
+// 2) Sigmoid Activation function
 // the default activation function is kept as sigmoid function.
 
 unsigned linearFunction(unsigned x) {
     return x;
-}
 
+}
 
 int thresholdFunction(unsigned x) {
     int threshold = 0;
@@ -53,11 +57,70 @@ int thresholdFunction(unsigned x) {
 float sigmoidFunction(float x) {
     float exponential;
     float sigmoid;
-
     exponential = exp(-x);
     sigmoid = 1 / (1 + exponential);
-
     return sigmoid;
 }
+
+void setWeights(unsigned int weight_1, unsigned int weight_2) {
+    weight1 = weight_1;
+    weight_2 = weight_2;
+}
+
+
+int throwActivationFunction(string activeFunc) {
+    int switchCase = 0;
+    string actFunc = move(activeFunc);
+    if (actFunc == "sigmoid" || actFunc.empty()) {
+        switchCase = 0;
+        cout << "[:)] Running with Sigmoid Function as Activation Function." << endl;
+    } else if (actFunc == "linear") {
+        switchCase = 1;
+        cout << "[:)] Running with Linear Function as Activation Function." << endl;
+    } else if (actFunc == "threshold") {
+        switchCase = 2;
+        cout << "[:)] Running with Threshold Function as Activation Function." << endl;
+    }
+
+    return switchCase;
+}
+
+int notGate(int input, const string &activationFunction) {
+
+    int answer;
+    unsigned calc;
+    const string &actFunc = activationFunction;
+    try {
+        if (input == 1 || input == 0) {
+
+        } else {
+            throw 101;
+        }
+    }
+    catch (int err) {
+        cout << "[!] Error: Input for not gate should be Binary, either 1 or 0." << endl << "Exited with error code:"
+             << err << endl;
+    }
+
+    calc = (weight1 * input) - threshold_value;
+
+    switch (throwActivationFunction(actFunc)) {
+        case 0:
+            answer = sigmoidFunction(calc);
+        case 1:
+            answer = linearFunction(calc);
+        case 2:
+            answer = thresholdFunction(calc);
+    }
+
+    return answer;
+}
+
+
+int andGate(int input1, int input2) {
+
+    return 0;
+}
+
 
 #endif //BIN_MCCULLOCHPITTS_NEURONMODEL_H
--- bin/main.cpp
+++ bin/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "McCullochPitts_NeuronModel.h"
 using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
+    setWeights(-15,-15);
+    cout<< notGate(0,"linear");
     return 0;
 }
